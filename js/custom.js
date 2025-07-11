17df57b5496b1c23dc1a97cafea2d708a444a29b
Taoufik JABBARI <j.taoufik@outlook.com>
2025-07-11 10:56:18 +0200

    Remove unused assets and files for cleanup and optimize script usage

--- js/custom.js
+++ js/custom.js
@@ -6,17 +6,3 @@ function getYear() {
 }
 
 getYear();
-
-
-// Testimonials are now displayed in a static grid (2 per row)
-
-
-
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
